8317f33045a5f2cd2e26964b5ef4156631d76b1c
Lankalapalli M P S Devi Pavan <128669142+Pav125@users.noreply.github.com>
2024-05-20 17:09:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://devipavan824:oPch1bslYgCac5Lm@backenddb.wpgtwoh.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+    "mongodb+srv://devipavan824:<password>@backenddb.wpgtwoh.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
   )
   .then(() => {
     console.log("Connected to database!");
